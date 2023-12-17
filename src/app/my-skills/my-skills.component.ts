185ca3452a5661b5135d9449d1c72d4b957570c8
Johannes Springer <johannes.springer.01@gmx.de>
2023-12-17 16:18:06 +0100

    added my skills

--- src/app/my-skills/my-skills.component.ts
+++ src/app/my-skills/my-skills.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-my-skills',
   templateUrl: './my-skills.component.html',
   styleUrls: ['./my-skills.component.scss']
 })
-export class MySkillsComponent {
+export class MySkillsComponent implements OnInit {
+  @Input() skills = [
+    ['Angular', 'angular-icon'],
+    ['Typescript', 'typescript-icon'],
+    ['JavaScript', 'javascript-icon'],
+    ['HTML', 'html-icon'],
+    ['CSS', 'css-icon'],
+    ['Firebase', 'firebase-icon'],
+    ['Git', 'git-icon'],
+    ['Rest-Api', 'api-icon'],
+    ['Scrum', 'scrum-icon'],
+    ['Material Design', 'material-design-icon'],
+  ];
+
+  ngOnInit() : void {
+
+  }
 
 }
