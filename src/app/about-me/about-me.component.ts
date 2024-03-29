46ce25dfb801a3c4fe63ed362ff1e20a2115b0b7
JohannesSpringer <johannes.springer.01@gmx.de>
2024-03-29 16:18:32 +0100

    adde about me animation and background

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -1,10 +1,32 @@
+import { animate, animation, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about-me',
   templateUrl: './about-me.component.html',
-  styleUrls: ['./about-me.component.scss']
+  styleUrls: ['./about-me.component.scss'],
+  animations: [
+    trigger('meImg', [
+      state('normal', style({
+        opacity: '0',
+        top: '0',
+        left: '0'
+      })),
+      state('hovered', style({
+        opacity: '1',
+        top: '20px',
+        left: '20px'
+      })),
+      transition('normal => hovered', [
+        animate('100ms ease-in'),
+      ])
+    ])
+  ]
 })
 export class AboutMeComponent {
+  isHovered: boolean = false;
 
+  setHover() {
+    this.isHovered = true;
+  }
 }
