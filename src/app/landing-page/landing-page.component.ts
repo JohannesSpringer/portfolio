7d4767d9d69ba803ea83408d72cf04fde178ebbe
Johannes Springer <johannes.springer.01@gmx.de>
2024-07-09 16:35:58 +0200

    feature/ dynamic calculation of height in landing page

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
-export class LandingPageComponent {
+export class LandingPageComponent implements AfterViewInit {
+  headline: HTMLElement | null = null;
+  contactLink: HTMLElement | null = null;
 
+  constructor(private renderer: Renderer2) { }
+
+  ngAfterViewInit() {
+    this.headline = document.getElementById("headline");
+    this.contactLink = document.getElementById("contactLink");
+    this.adjustContentHeight();
+    window.addEventListener('resize', this.adjustContentHeight.bind(this));
+  }
+
+  adjustContentHeight() {
+    if (this.headline && this.contactLink) {
+      const headlineHeight = this.headline.offsetHeight;
+      const contactLinkHeight = window.innerHeight - headlineHeight;
+      this.renderer.setStyle(this.contactLink, 'height', `calc(${contactLinkHeight}px - 218px)`);
+    }
+  }
 }
