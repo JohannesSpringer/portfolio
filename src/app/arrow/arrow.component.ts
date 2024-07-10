08c4a0c9e18c15879642199dc51f04971f929acb
Johannes Springer <johannes.springer.01@gmx.de>
2024-07-10 15:12:56 +0200

    feature arrow animation dependent on scroll position

--- src/app/arrow/arrow.component.ts
+++ src/app/arrow/arrow.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, HostListener, Input } from '@angular/core';
+import { SharedService } from '../shared.service';
 
 @Component({
   selector: 'app-arrow',
@@ -9,7 +10,33 @@ export class ArrowComponent {
   @Input() inverted: boolean = false;
   @Input() hovered: boolean = false;
 
+  constructor(private sharedService: SharedService, private element: ElementRef) {}
+
   setClass() {
     this.hovered = true;
+  }
+
+  resetClass() {
+    this.hovered = false;
+  }
+
+  /**
+   * Creating Listener for scroll event to check elements visibility in viewport
+   * @param event 
+   */
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(event: Event) {    
+    this.checkElementPosition();
+  }
+
+  /**
+   * Check visibility for all portfolio projects in viewport and show all information of visible project
+   */
+  checkElementPosition() {
+    this.hovered = this.elementIsVisible(this.element.nativeElement);
+  }
+
+  elementIsVisible(ele: HTMLElement) {
+    return this.sharedService.elementIsVisible(ele);
   }
 }
